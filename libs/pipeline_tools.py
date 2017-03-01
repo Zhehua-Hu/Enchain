d1b18b07867da6466f654e43acd6a023bf69fccf
Zhehua-Hu <ZhehuaHu@163.com>
2017-03-01 10:48:38 +0800

    try

---
+++ libs/pipeline_tools.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# coding=utf-8
+""" """
+
+import os
+
+def make_perfect_path(path):
+	if type(path) != str:
+		print('"%s" is not valid path!' % path)
+		return
+	if path[-1] == '/':
+		return path[:-1]
+	return path
+
+
+def make_if_not_exist(path):
+	if not os.path.exists(path):
+		os.system("mkdir " + path)
+	return path
+
+
+def find_suffix_files(path, suffix):
+	ret = []
+	for root, dirs, filenames in os.walk(path):
+		for filename in filenames:
+			if not filename.startswith('.'): # not hiden file
+				if filename.endswith(suffix):
+					ret.append(filename)
+	return ret
